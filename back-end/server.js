0fea7ac634ea53193f06b1b547d29a5dcf05aed1
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2025-01-03 20:26:11 -0300

    Fix server

--- back-end/server.js
+++ back-end/server.js
@@ -1,20 +1,25 @@
 import express from "express";
-import { config } from "dotenv";
-import { connectDB } from "./database/db.js";
+import dotenv from "dotenv";
 
-config();
+import { connectDB } from "./database/db.js";
+import router from "./routes/product.route.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT;
 const BASE_URL = process.env.BASE_URL;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({status: "ok"});
-})
+app.use(express.json());
 
+app.use('/api/products', router);
 
-app.listen(PORT, () => {
-    console.info(`Server is running at ${BASE_URL}${PORT}`);
-    console.info(`Server change again`);
+app.listen(PORT, async () => {
+    try {
+        await connectDB();
+        console.info(`Server is running at ${BASE_URL}${PORT}`);
+    } catch (error) {
+        console.error(error.message);      
+    }
 })
