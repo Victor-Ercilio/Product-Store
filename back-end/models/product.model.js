95ebbe8241b472d19f91aef945cdb8d648acf119
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2025-01-03 20:25:23 -0300

    Add product model

--- back-end/models/product.model.js
+++ back-end/models/product.model.js
@@ -1,4 +1,22 @@
 import mongoose from "mongoose";
 
-console.log("ok")
-console.log("oksad")
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
